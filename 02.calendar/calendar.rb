761ab94c761fa21c78ad587ebba87a6e56312ae5
tosan777 <tosan.golang1.14.on@gmail.com>
2022-05-24 10:34:13 +0900

    2つ目にいただいたレビュー以外の修正を対応しました

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -3,62 +3,54 @@ require 'optparse'
 
 
 def optparse
- opt = OptionParser.new
+  opt = OptionParser.new
 
- now = Time.now
+  now = Time.now
 
- opt.on('-m [m]') do |m|
-  if m == nil
-   @month = now.month
-  else
-   @month = m.to_i
+  opt.on('-m [m]') do |m|
+    if m == nil
+       @month = now.month
+     else
+       @month = m.to_i
+    end
   end
- end
 
- opt.on('-y [y]') do |y|
-  if y == nil
-   @year =now.year
-  else
-   @year = y.to_i
+  opt.on('-y [y]') do |y|
+    if y == nil
+      @year = now.year
+    else
+      @year = y.to_i
+    end
   end
- end
 
- opt.parse!(ARGV)
+  opt.parse!(ARGV)
 end
 
 
 def calendar(optparse)
- first_day = Date.new(@year, @month, 1).day
- last_day = Date.new(@year, @month, -1).day
 
- print "#{@month}月 #{@year}年".center(20)
- puts "\n"
+  first_day = Date.new(@year, @month, 1).day
+  last_day = Date.new(@year, @month, -1).day
 
- youbi = ["日","月","火","水","木","金","土"]
+  print "#{@month}月 #{@year}年".center(20)
+  puts "\n"
 
- youbi.each do |y|
-  print y.rjust(2)
-  if y == "土"
-   puts "\n"
+  day_of_week = ["日","月","火","水","木","金","土"]
+  puts day_of_week.join(' ')
+
+  first_day = Date.new(@year, @month, 1).day
+  last_day = Date.new(@year, @month, -1).day
+  first_wday = Date.new(@year, @month, 1).wday
+  day_of_week[first_wday]
+
+  (first_wday).times do |t|
+    print "   " * first_day
   end
- end
 
-
- first_day = Date.new(@year, @month, 1).day
- last_day = Date.new(@year, @month, -1).day
- first_wday = Date.new(@year, @month, 1).wday
- youbi[first_wday]
-
- (first_wday).times do |t|
-  print "   "
- end
-
- (first_day..last_day).each do |d|
-  printf("%3d", d)
-  if (d + first_wday) % 7 === 0
-   puts "\n"
+  (first_day..last_day).each do |d|
+    printf d.to_s.rjust(3)
+    puts "\n" if (d + first_wday) % 7 === 0
   end
- end
 end
 
 
