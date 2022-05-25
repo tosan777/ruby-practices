87b51a22691ffd593bdb62c6b1f2ccefadad2f63
tosan777 <tosan.golang1.14.on@gmail.com>
2022-05-25 09:10:43 +0900

    ボウリングスコアの計算ができるようになりました

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
- 
+
 require 'rubygems'
 require 'bundler/setup'
 require 'debug'
@@ -21,25 +21,25 @@ shots.each_slice(2) do |s|
   frames << s
 end
 
-
 point = 0
 frames.each_with_index do |frame, index|
- if frames[index].sum == 10 && frames[index + 1].sum == 10
-   point += frames[index].sum + frames[index + 1].sum + frames[index + 2].first
- elsif frame[0] == 10
-   point += 10 + (frames[index + 1].sum)
- elsif frame.sum == 10 
-   point += frame.sum + (frames[index + 1].first)
- else
-   point += frame.sum
- end
-
- if frames[index] == 9 && frames[index].first == 10
-   point += (frames[index] >> frames[index + 1]).sum
- elsif frames[index] == 9 && frames[index].sum == 10
-   point += (frames[index] >> frames[index + 1]).sum
- else
-   ""
- end 
+  if index == 9 && frame[0] == 10 && frames[index + 1].first == 10
+    point += frame[0] + frames[index + 1].first + frames[index + 1].first
+    break
+  elsif index == 9 && frame[0] == 10
+    point += frame[0] + frames[index + 1].sum
+    break
+  elsif index == 9 && frame.sum == 10
+    point += frame.sum + frames[index + 1].first
+    break
+  elsif frame[0] == 10 && frames[index + 1].first == 10
+    point += frame[0] + frames[index + 1].first + frames[index + 2].first
+  elsif frame[0] == 10
+    point += 10 + frames[index + 1].sum
+  elsif frame.sum == 10
+    point += frame.sum + frames[index + 1].first
+  else
+    point += frame.sum
+  end
 end
 puts point
