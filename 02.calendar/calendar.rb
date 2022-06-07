35cf45f594cd2bc53d1a345466b508b6c529a3a9
tosan777 <tosan.golang1.14.on@gmail.com>
2022-06-07 11:48:41 +0900

    引数に指定した値をローカル変数に格納し、それが戻り値となるoptparseメソッドを作成しました

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -6,39 +6,41 @@ def optparse
 
   now = Time.now
 
-  opt.on('-m [m]') do |m|
-    @month = m.to_i || now.month
+  month = opt.on('-m [m]') do |m|
+    month = m || now.month
   end
 
-  opt.on('-y [y]') do |y|
-    @year = y.to_i || now.year
+  year = opt.on('-y [y]') do |y|
+    year = y || now.year
   end
 
   opt.parse!(ARGV)
+  return month, year
 end
 
 def calendar(optparse)
-  binding.irb
-  first_day = Date.new(@year, @month, 1).day
-  last_day = Date.new(@year, @month, -1).day
+  month = optparse[0].to_i
+  year = optparse[1].to_i
+  first_day = Date.new(year, month, 1).day
+  last_day = Date.new(year, month, -1).day
    
-  print "#{@month}月 #{@year}年".center(20)
-    puts "\n"
+  print "#{month}月 #{year}年".center(20)
+  puts "\n"
     
-   day_of_week = ["日","月","火","水","木","金","土"]
-   puts day_of_week.join(' ')
+  day_of_week = ["日","月","火","水","木","金","土"]
+  puts day_of_week.join(' ')
    
-   first_day = Date.new(@year, @month, 1).day
-   last_day = Date.new(@year, @month, -1).day
-   first_wday = Date.new(@year, @month, 1).wday
-   day_of_week[first_wday]
+  first_day = Date.new(year, month, 1).day
+  last_day = Date.new(year, month, -1).day
+  first_wday = Date.new(year, month, 1).wday
+  day_of_week[first_wday]
 
-   print "   " * first_wday 
+  print "   " * first_wday 
  
-   (first_day..last_day).each do |d|
-     printf d.to_s.rjust(3)
-     puts "\n" if (d + first_wday) % 7 === 0
-   end
+  (first_day..last_day).each do |d|
+    printf d.to_s.rjust(3)
+    puts "\n" if (d + first_wday) % 7 === 0
+  end
 end
  
 calendar(optparse)
