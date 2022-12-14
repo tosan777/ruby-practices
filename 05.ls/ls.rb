e31e1372512eedd63641d8efacbd9ff6e8b473bf
tosan777 <ksk.pleiades@gmail.com>
2022-12-14 11:11:25 +0900

    連想配列の要素数が3つだった場合、３列で綺麗に出力できるようになった。
    要素数が増えると崩れてしまう

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,15 +1,28 @@
 #!/usr/bin/env ruby
+require 'pry'
 
-array = []
-
-Dir.foreach('.') do |item|
-  next if item == '.' or item == '..'
-  array << item
+def find_dir
+  array = []
+  Dir.foreach('.') do |item|
+    next if item.match(/\A\.+/)
+    array << item
+  end
+  array
 end
 
-binding.irb
-
-array.each_with_index do |item, index|
-  puts "\n" if index % 3 == 0
-  print item.ljust(25)
+def show_dir
+  dir = find_dir.sort.each_slice(3).to_a
+  max = dir.max_by { |a| a.size }.size
+  adjust_dir = dir.each do |a|
+              while a.size < max
+                a << nil
+              end
+            end
+  trans_dir = adjust_dir.transpose.flatten
+  trans_dir.each_with_index do |item, index|
+    next if item == nil
+    print (index + 1) % 3 == 0 ? "#{item}\n" : item.ljust(24)
+  end
 end
+
+show_dir
