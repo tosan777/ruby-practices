02a3209a3f55467f09785b33d3108429ed5b3847
tosan777 <ksk.pleiades@gmail.com>
2022-12-15 13:37:21 +0900

    rubocopを通しました。

--- 07.bowling_object/game.rb
+++ 07.bowling_object/game.rb
@@ -20,15 +20,16 @@ class Game
     score = 0
     frames.each_with_index do |frame, index|
       return score if index == 10
-      if frame.first == 'X' && frames[index + 1].first == 'X'
-        score += Frame.new(frame.first, frames[index + 1].first, frames[index + 2].first).score
-      elsif frame.first == 'X'
-        score += Frame.new(frame.first, frames[index + 1].first, frames[index + 1].last).score
-      elsif frame.first.to_i + frame.last.to_i == 10
-        score += Frame.new(frame.first, frame.last, frames[index + 1].first).score
-      else
-        score += Frame.new(frame.first, frame.last).score
-      end
+
+      score += if frame.first == 'X' && frames[index + 1].first == 'X'
+                 Frame.new(frame.first, frames[index + 1].first, frames[index + 2].first).score
+               elsif frame.first == 'X'
+                 Frame.new(frame.first, frames[index + 1].first, frames[index + 1].last).score
+               elsif frame.first.to_i + frame.last.to_i == 10
+                 Frame.new(frame.first, frame.last, frames[index + 1].first).score
+               else
+                 Frame.new(frame.first, frame.last).score
+               end
     end
     score
   end
