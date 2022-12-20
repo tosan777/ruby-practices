b1a310834922cbe0ed8d8ec9e0e5585454365ae8
tosan777 <ksk.pleiades@gmail.com>
2022-12-20 11:10:05 +0900

    rubocopを通しました。

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,32 +1,35 @@
-#!/usr/bin/env ruby
+# frozen_string_literal: true
+
 require 'pry'
 
 def find_dir
   array = []
   Dir.foreach('.') do |item|
-    next if item.match(/\A\.+/)
+    next if /\A\.+/.match?(item)
+
     array << item
   end
   array
 end
 
-def show_dir
+def adjust_dir
   dir = if find_dir.size % 4 == 3 || find_dir.size % 4 == 2
           find_dir.sort.each_slice(4).to_a
         else
           find_dir.sort.each_slice(3).to_a
         end
-  max = dir.max_by { |a| a.size }.size
-  adjust_dir = dir.each do |a|
-    while a.size < max
-      a << nil
-    end
-  end
-  trans_dir = adjust_dir.transpose.flatten
-  trans_dir.each_with_index do |item, index|
-    print ' ' if item == nil
-    print (index + 1) % 3 == 0 ? "#{item}\n" : item.ljust(24)
+  max = dir.max_by(&:size).size
+  dir.each do |a|
+    a << nil while a.size < max
   end
 end
 
-show_dir
+def trans_dir
+  trans_dir = adjust_dir.transpose.flatten
+  trans_dir.each_with_index do |item, index|
+    print ' ' if item.nil?
+    print ((index + 1) % 3).zero? ? "#{item}\n" : item.ljust(24)
+  end
+end
+
+trans_dir
