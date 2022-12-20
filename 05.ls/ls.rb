3bde0979cf736caba6e5d890c8ffe5a46b8c4887
tosan777 <ksk.pleiades@gmail.com>
2022-12-20 10:56:44 +0900

    lsコマンドの作成

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -11,16 +11,20 @@ def find_dir
 end
 
 def show_dir
-  dir = find_dir.sort.each_slice(3).to_a
+  dir = if find_dir.size % 4 == 3 || find_dir.size % 4 == 2
+          find_dir.sort.each_slice(4).to_a
+        else
+          find_dir.sort.each_slice(3).to_a
+        end
   max = dir.max_by { |a| a.size }.size
   adjust_dir = dir.each do |a|
-              while a.size < max
-                a << nil
-              end
-            end
+    while a.size < max
+      a << nil
+    end
+  end
   trans_dir = adjust_dir.transpose.flatten
   trans_dir.each_with_index do |item, index|
-    next if item == nil
+    print ' ' if item == nil
     print (index + 1) % 3 == 0 ? "#{item}\n" : item.ljust(24)
   end
 end
