ae210b5cc036d5afb2cf322a283851c67f95b9cf
tosan777 <ksk.pleiades@gmail.com>
2022-12-26 15:42:23 +0900

    ストライクかスペアを判断できるインスタンスメソッドを作成しました。

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -3,13 +3,20 @@
 require './shot'
 
 class Frame
-  def initialize(first_mark, second_mark = nil, third_mark = nil)
+  def initialize(first_mark, second_mark = nil)
     @first_shot  = Shot.new(first_mark)
     @second_shot = Shot.new(second_mark)
-    @third_shot  = Shot.new(third_mark)
+  end
+
+  def strike?
+    @first_shot.score == 10
+  end
+
+  def spare?
+    (@first_shot.score + @second_shot.score) == 10
   end
 
   def score
-    @first_shot.score + @second_shot.score + @third_shot.score
+    @first_shot.score + @second_shot.score
   end
 end
--- 07.bowling_object/game.rb
+++ 07.bowling_object/game.rb
@@ -20,16 +20,17 @@ class Game
     score = 0
     frames.each_with_index do |frame, index|
       return score if index == 10
+      frame = Frame.new(frame[0], frame[1])
 
-      score += if frame.first == 'X' && frames[index + 1].first == 'X'
-                 Frame.new(frame.first, frames[index + 1].first, frames[index + 2].first).score
-               elsif frame.first == 'X'
-                 Frame.new(frame.first, frames[index + 1].first, frames[index + 1].last).score
-               elsif frame.first.to_i + frame.last.to_i == 10
-                 Frame.new(frame.first, frame.last, frames[index + 1].first).score
-               else
-                 Frame.new(frame.first, frame.last).score
-               end
+      score += if frame.strike? && Frame.new(frames[index + 1].first).strike?
+                frame.score + Frame.new(frames[index + 1].first, frames[index + 2].first).score
+              elsif frame.strike?
+                frame.score + Frame.new(frames[index + 1].first, frames[index + 1].last).score
+              elsif frame.spare?
+                frame.score + Frame.new(frames[index + 1].first).score
+              else
+                frame.score
+              end
     end
     score
   end
