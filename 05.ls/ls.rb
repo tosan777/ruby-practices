1f003fada8ad9fdf7fad78e831a66370855ea6f0
tosan777 <ksk.pleiades@gmail.com>
2023-01-12 09:20:44 +0900

    rubocopを通しました。

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -8,25 +8,26 @@ def find_dir
   array
 end
 
-def adjust_dir
+def slice_dir
   column = 3
   dir_slice = find_dir.size / column
   sort_dir = find_dir.sort
-  dir = if (sort_dir.size % column).zero?
-          sort_dir.each_slice(dir_slice).to_a
-        else
-          sort_dir.each_slice(dir_slice + 1).to_a
-        end
-  max = dir.max_by(&:size).size
-  dir.each do |a|
-    a << '' while a.size < max
+  if (sort_dir.size % column).zero?
+    sort_dir.each_slice(dir_slice).to_a
+  else
+    sort_dir.each_slice(dir_slice + 1).to_a
   end
+end
+
+def adjust_dir
+  max = slice_dir.max_by(&:size).size
+  slice_dir.each { |a| a << '' while a.size < max }
 end
 
 def trans_dir
   trans_dir = adjust_dir.transpose
   trans_dir.each do |array|
-    adjust_array = array.map {|item| item.ljust(24)}.join
+    adjust_array = array.map { |item| item.ljust(24) }.join
     print "#{adjust_array}\n"
   end
 end
