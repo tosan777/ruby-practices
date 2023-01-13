12c0f5b5de761abd369c6f69e20f0cd8f0cf7b2a
tosan777 <ksk.pleiades@gmail.com>
2023-01-13 11:19:19 +0900

    arrayだと抽象的であるため、変数名を変更しました。

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -27,9 +27,9 @@ end
 
 def trans_dir
   trans_dir = adjust_dir.transpose
-  trans_dir.each do |array|
-    adjust_array = array.map { |item| item.ljust(24) }.join
-    print "#{adjust_array}\n"
+  trans_dir.each do |trans_dir_item|
+    adjust_dir_item = trans_dir_item.map { |item| item.ljust(24) }.join
+    print "#{adjust_dir_item}\n"
   end
 end
 
